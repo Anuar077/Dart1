b310ca9cda091d943c2ce1513251aa6ac420e6b8
Anuar077 <Anuar077@users.noreply.github.com>
2021-03-03 22:14:11 +0600

    Update

--- .idea/main.dart
+++ .idea/main.dart
@@ -1,19 +1,18 @@
 main() {
-    list ('Almaty','Aktau','Oral','Oskemen','Semey');
-  }
+  list ('Almaty','Aktau','Oral','Oskemen','Semey');
+}
 
-  void list(String a,String b, String c, String d, String e){
-    var len = [a,b,c,d,e];
-    var big = len[0].length;
-    print(len);
-    for (int i =0;i <len.length;i++){
-      if (len[i].length >big) {
-        big = len[i].length;
-      }
+void list(String a,String b, String c, String d, String e){
+  var len = [a,b,c,d,e];
+  var NewList=[];
+  var big = len[0].length;
+  print(len);
+  for (int i =0;i <len.length;i++){
+    if (len[i].length >big) {
+      big = len[i].length;
+      NewList.add(i);
     }
-    print(big);
   }
-
-
-
-
+  print(big);
+  print(NewList);
+}
